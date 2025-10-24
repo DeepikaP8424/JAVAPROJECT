8ef4b5623a8dc1638ba08075ab51c02038a1680a
DeepikaP8424 <deepikap8424@gmail.com>
2025-10-24 16:31:41 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.agri.AGRIBOT;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		        ChatBot bot = new ChatBot();
+		        bot.startChat();
+		    }
+		
+
+	}
+
